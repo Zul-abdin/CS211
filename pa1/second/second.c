804d8bb752743fa6b59c7cc9773416238673c208
Zain UlAbdin <zulabdin.ctr@ondeck.com>
2019-07-02 16:58:16 -0400

    pa1/second: Additional unsure adds

--- pa1/second/second.c
+++ pa1/second/second.c
@@ -8,48 +8,37 @@ struct Node{
     int val;
 };
 
-void insertNode(struct Node*, int val);
+void insertNode(int val);
 int hashF(int);
+struct Node hashMap[9999];
 
 int main(int argc, char** argv) {
 
     if(argc != 2){
         printf("Error: No File Given\n");
-        return 0;
+        //return 0;
     }
-
-    struct Node* hashMap[9999];
-
-    insertNode(hashMap[hashF(1)], 1);
-    insertNode(hashMap[hashF(4)], 1);
-
-
-
-
     //hash function: key modulo the number of buckets.
 
-    int key = 5;
-    int index = key % 10000;
-
-
-    printf("%d\n", key);
+    insertNode(4);
+    printf("%d\n", hashMap[4].head->val);
 
 }
 
-void insertNode(struct Node* hM, int key){
-    struct Node* n = (struct Node*)malloc(sizeof(struct Node)); //Survives outside function b/c malloc
+void insertNode(int key){
 
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node)); //Survives outside function b/c malloc
     int index = hashF(key);
 
     n -> next = NULL;
     n -> val = key;
 
-    if(hM[index].tail == NULL){
-        hM[index].head = n;
-        hM[index].tail = n;
+    if(hashMap[index].head == NULL){
+        hashMap[index].head = n;
+        hashMap[index].tail = n;
     } else {
-        hM[index].tail -> next = n;
-        hM[index].tail = n;
+        hashMap[index].tail -> next = n;
+        hashMap[index].tail = n; //equal to tail = tail -> next
     }
 
 }
