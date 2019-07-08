fc94652eb6bb473bef0ed3aa8c9315cc0dfaaa3b
zulabdin <zulabdin.ctr@ondeck.com>
2019-07-08 14:10:51 -0400

    Finished third: Works with test1.txt

--- pa1/third/third.c
+++ pa1/third/third.c
@@ -1,6 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int get(unsigned short x, int n);
+unsigned short comp(unsigned short x, int n);
+unsigned short set(unsigned short x, int n, int v);
+
+int main(int argc, char** argv) {
+
+    if (argc != 2) {
+        printf("Error: No File Given.\n");
+        return 0;
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+
+    unsigned short x = 0;
+
+    fscanf(fp, "%hu\n", &x);
+
+    while(!feof(fp)){
+        char c = 0;
+        int n = 0;
+        int v = 0;
+        fscanf(fp, "%c", &c);
+        if(c == 'g'){
+            fscanf(fp, "%c", &c);
+            fscanf(fp, "%c", &c);
+            fscanf(fp, " %d ", &n);
+            printf("%d\n", get(x, n));
+        } else if(c == 'c'){
+            fscanf(fp, "%c", &c);
+            fscanf(fp, "%c", &c);
+            fscanf(fp, "%c", &c);
+            fscanf(fp, " %d ", &n);
+            fscanf(fp, " %d ", &v);
+            x = comp(x, n);
+            printf("%d\n", x);
+        } else if(c == 's') {
+            fscanf(fp, "%c", &c);
+            fscanf(fp, "%c", &c);
+            fscanf(fp, " %d ", &n);
+            x = set(x, n, v);
+            printf("%d\n", x);
+        }
+    }
+}
+
+int get(unsigned short x, int n){
+    return (x >> n) & 1;
+}
+
+unsigned short comp(unsigned short x, int n){
+    return (1 << n) ^ x;
+}
+
+unsigned short set(unsigned short x, int n, int v){
+    if(v == 0){
+        return (1 << n) | x;
+    } else {
+        return (254 << n) & x;
+    }
 }
